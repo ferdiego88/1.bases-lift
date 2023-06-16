b253bb1d3be7e6633c24df877cdde324f1e9861c
Fernando Santillan <diego.santillan@pragma.com.co>
2023-06-15 21:38:16 -0500

    feat: add form character

--- src/app/shared/components/molecules/form-character/form-character.component.ts
+++ src/app/shared/components/molecules/form-character/form-character.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-character',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class FormCharacterComponent {
 
+  @Input() className = '';
+  @Input() titleForm = '';
 }
