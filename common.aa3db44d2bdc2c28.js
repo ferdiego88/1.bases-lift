00cac38ce4fa4b2ea9d0e6027a2db70527e4add5
Fernando Santillan <diego.santillan@pragma.com.co>
2023-07-20 15:01:29 -0500

    Auto-generated commit [ci skip]

---
+++ common.aa3db44d2bdc2c28.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbases=self.webpackChunkbases||[]).push([[592],{2340:(u,s,e)=>{e.d(s,{N:()=>t});const t={BASE_URL:"https://api.giphy.com",BASE_COUNTRY_URL:"https://restcountries.com/v3.1",GIFS_API_TOKEN:"7bF9631PZyGG68PUzirO17S2CT1zuLTV"}}}]);
