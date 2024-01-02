d4cd6b9004427c405074aae11a2b2c8659a61547
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-01-02 17:18:54 -0500

    feat: i18nSelect y i18nPlural

--- src/app/modules/pipes-app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/modules/pipes-app/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18n Select
+  public name = 'Fernando';
+  public gender: 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    'male' : 'invitarlo',
+    'female': 'invitarla'
+  }
+
+
+
+  changeClient() {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+
+  // i18nPlural
+
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Melissa', 'Eduardo', 'Natalia', 'Juan'];
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+
+
+  deleteClient() {
+    this.clients.shift();
+  }
+
 }
