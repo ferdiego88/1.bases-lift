cd4595d78351ff76f44ff7edb14af314c907e49d
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-01-24 07:50:43 -0500

    feat: pipe Personalizado

--- src/app/modules/pipes-app/pipes-app.module.ts
+++ src/app/modules/pipes-app/pipes-app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 import { PipesAppRoutingModule } from './pipes-app-routing.module';
-import { PipesMainComponent } from './pages/pipes-main/pipes-main.component';
+
+import { AtomsModule } from '@app/shared/components/atoms/atoms.module';
 import { MoleculesModule } from '@app/shared/components/molecules/molecules.module';
+
+import { PipesMainComponent } from './pages/pipes-main/pipes-main.component';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '@app/shared/prime-ng/prime-ng.module';
-import { AtomsModule } from '@app/shared/components/atoms/atoms.module';
+import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pages/pipes/toggle-case.pipe';
 
 
 
@@ -18,14 +20,16 @@ import { AtomsModule } from '@app/shared/components/atoms/atoms.module';
     PipesMainComponent,
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent,
+    ToggleCasePipe
   ],
   imports: [
     CommonModule,
+    PrimeNgModule,
     PipesAppRoutingModule,
     AtomsModule,
     MoleculesModule,
-    PrimeNgModule
   ]
 })
 export class PipesAppModule { }
