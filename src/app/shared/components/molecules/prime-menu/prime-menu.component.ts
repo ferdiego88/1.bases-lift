1416abd4af99d2bcb30d314b47d0e46e411f034f
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-07-07 14:50:48 -0500

    feat: heroe

--- src/app/shared/components/molecules/prime-menu/prime-menu.component.ts
+++ src/app/shared/components/molecules/prime-menu/prime-menu.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-prime-menu',
   templateUrl: './prime-menu.component.html',
   styleUrls: ['./prime-menu.component.scss']
 })
 export class PrimeMenuComponent  {
-
-  @Input() menuItems: MenuItem[] | undefined;
 
 
 }
